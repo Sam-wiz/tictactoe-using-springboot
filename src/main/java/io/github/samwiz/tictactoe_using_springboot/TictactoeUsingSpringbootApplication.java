28953abcb8239872835eb6ab9909b4b4803bead8
Sam-wiz <samrudhvvandakudri2005@gmail.com>
2024-04-23 10:36:30 +0530

    Intialised the repo with basic spring files

---
+++ src/main/java/io/github/samwiz/tictactoe_using_springboot/TictactoeUsingSpringbootApplication.java
@@ -0,0 +1,13 @@
+package io.github.samwiz.tictactoe_using_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TictactoeUsingSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TictactoeUsingSpringbootApplication.class, args);
+    }
+
+}
